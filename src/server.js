4e1e1e75818b279e66220e00e7de659e7164b6b5
Ederson Machado <edersonmberti@gmail.com>
2020-01-17 00:23:04 -0300

    fix route path

--- src/server.js
+++ src/server.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(cors());
 app.use(routes);
 
-app.use("/", express.static(path.resolve(__dirname, "..", "uploads")));
+app.use("/files", express.static(path.resolve(__dirname, "..", "uploads")));
 
 app.listen(process.env.PORT, () => {
   console.log("UHUL! The API is UP && RUNNING!!!", process.env.PORT);
