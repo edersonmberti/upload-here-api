7c68410b6970375b853e30fc9447318c325757a8
Ederson Machado <edersonmberti@gmail.com>
2020-01-17 00:01:45 -0300

    added cors

--- src/server.js
+++ src/server.js
@@ -1,15 +1,17 @@
 const express = require("express");
 const path = require("path");
+const cors = require("cors");
 const routes = require("./routes");
 
 require("dotenv").config();
 
 const app = express();
 
+app.use(cors());
 app.use(routes);
 
 app.use("/", express.static(path.resolve(__dirname, "..", "uploads")));
 
 app.listen(process.env.PORT, () => {
-  console.log("UHUL! The API is UP && RUNNING!!!");
+  console.log("UHUL! The API is UP && RUNNING!!!", process.env.PORT);
 });
